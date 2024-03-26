ec3ac249f182d6afb8e99114a9a2843bd83e20d5
zeroupper <mikloshavlik@gmail.com>
2024-03-26 21:12:41 +0100

    fix: tweak action with position

--- domain/action.py
+++ domain/action.py
@@ -13,10 +13,10 @@ class ActionType(Enum):
 class Action(Enum):
     NoOp = ("NoOp", ActionType.NoOp, 0, 0, 0, 0)
 
-    MoveN = ("Move(N)", ActionType.Move, Position(-1, 0), 0, 0)
-    MoveS = ("Move(S)", ActionType.Move, Position(1, 0), 0, 0)
-    MoveE = ("Move(E)", ActionType.Move, Position(0, 1), 0, 0)
-    MoveW = ("Move(W)", ActionType.Move, Position(0, -1), 0, 0)
+    MoveN = ("Move(N)", ActionType.Move, Position(-1, 0), Position(0, 0))
+    MoveS = ("Move(S)", ActionType.Move, Position(1, 0), Position(0, 0))
+    MoveE = ("Move(E)", ActionType.Move, Position(0, 1), Position(0, 0))
+    MoveW = ("Move(W)", ActionType.Move, Position(0, -1), Position(0, 0))
     
     def __init__(self, name, type, aRPos, bRPos):
         self.name_ = name
