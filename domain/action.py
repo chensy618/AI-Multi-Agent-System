2439a2d66ab21d2f6a2ce47598fa54b038be3c92
chensy618 <146075999+chensy618@users.noreply.github.com>
2024-03-27 23:01:51 +0100

    update action.py, adding push and pull actions

--- domain/action.py
+++ domain/action.py
@@ -14,9 +14,36 @@ class Action(Enum):
     NoOp = ("NoOp", ActionType.NoOp, 0, 0, 0, 0)
 
     MoveN = ("Move(N)", ActionType.Move, Position(-1, 0), Position(0, 0))
-    MoveS = ("Move(S)", ActionType.Move, Position(1, 0), Position(0, 0))
-    MoveE = ("Move(E)", ActionType.Move, Position(0, 1), Position(0, 0))
+    MoveS = ("Move(S)", ActionType.Move, Position(1, 0),  Position(0, 0))
+    MoveE = ("Move(E)", ActionType.Move, Position(0, 1),  Position(0, 0))
     MoveW = ("Move(W)", ActionType.Move, Position(0, -1), Position(0, 0))
+    # Push actions
+    PushN  = ("Push(N,N)", ActionType.Push, Position(-1, 0), Position(-1, 0))
+    PushNE = ("Push(N,E)", ActionType.Push, Position(-1, 0), Position(0, 1))
+    PushNW = ("Push(N,W)", ActionType.Push, Position(-1, 0), Position(0, -1))
+    PushS  = ("Push(S,S)", ActionType.Push, Position(1, 0),  Position(1, 0))
+    PushSE = ("Push(S,E)", ActionType.Push, Position(1, 0),  Position(0, 1))
+    PushSW = ("Push(S,W)", ActionType.Push, Position(1, 0),  Position(0, -1))
+    PushE  = ("Push(E,E)", ActionType.Push, Position(0, 1),  Position(0, 1))
+    PushEN = ("Push(E,N)", ActionType.Push, Position(0, 1),  Position(-1, 0))
+    PushES = ("Push(E,S)", ActionType.Push, Position(0, 1),  Position(1, 0))
+    PushW  = ("Push(W,W)", ActionType.Push, Position(0, -1), Position(0, -1))
+    PushWN = ("Push(W,N)", ActionType.Push, Position(0, -1), Position(-1, 0))
+    PushWS = ("Push(W,S)", ActionType.Push, Position(0, -1), Position(1, 0))
+
+    # Pull actions
+    PullN  = ("Pull(N,N)", ActionType.Pull, Position(-1, 0), Position(-1, 0))
+    PullNE = ("Pull(N,E)", ActionType.Pull, Position(-1, 0), Position(0, 1))
+    PullNW = ("Pull(N,W)", ActionType.Pull, Position(-1, 0), Position(0, -1))
+    PullS  = ("Pull(S,S)", ActionType.Pull, Position(1, 0),  Position(1, 0))
+    PullSE = ("Pull(S,E)", ActionType.Pull, Position(1, 0),  Position(0, 1))
+    PullSW = ("Pull(S,W)", ActionType.Pull, Position(1, 0),  Position(0, -1))
+    PullE  = ("Pull(E,E)", ActionType.Pull, Position(0, 1),  Position(0, 1))
+    PullEN = ("Pull(E,N)", ActionType.Pull, Position(0, 1),  Position(-1, 0))
+    PullES = ("Pull(E,S)", ActionType.Pull, Position(0, 1),  Position(1, 0))
+    PullW  = ("Pull(W,W)", ActionType.Pull, Position(0, -1), Position(0, -1))
+    PullWN = ("Pull(W,N)", ActionType.Pull, Position(0, -1), Position(-1, 0))
+    PullWS = ("Pull(W,S)", ActionType.Pull, Position(0, -1), Position(1, 0))
     
     def __init__(self, name, type, aRPos, bRPos):
         self.name_ = name
