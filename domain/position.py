492c83c11ce0a874c95e26d6b3fb7511fcd8be32
zeroupper <mikloshavlik@gmail.com>
2024-04-02 17:24:18 +0200

    fix: use relative pos

--- domain/position.py
+++ domain/position.py
@@ -5,3 +5,22 @@ class Position:
 
     def __repr__(self):
         return f"Position(x={self.x}, y={self.y})"
+    
+    def __add__(self, other):
+        if isinstance(other, Position):
+            return Position(self.x + other.x, self.y + other.y)
+        else:
+            raise ValueError("Cannot add Position with type {}".format(type(other)))
+        
+    def __eq__(self, other):
+        if isinstance(other, Position):
+            return self.x == other.x and self.y == other.y
+        return False
+    
+    def __hash__(self):
+        return hash((self.x, self.y))
+    
+    def __sub__(self, other):
+        if isinstance(other, Position):
+            return Position(self.x - other.x, self.y - other.y)
+
