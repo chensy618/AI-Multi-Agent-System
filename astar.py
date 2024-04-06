10d6b5492f3378a84dc799d55340c5866778a54b
iamkashish <vermakashish888@gmail.com>
2024-04-06 17:24:04 +0200

    added multi-agents and boxes

--- astar.py
+++ astar.py
@@ -99,14 +99,22 @@ class PriorityQueue:
     
 class Heuristic(metaclass=ABCMeta):
     def __init__(self, initial_state: 'State'):
-        self.agent_goal_position = initial_state.goals[0].pos
-        #self.box_goal_position = {}
-        
+        all_goals = initial_state.goals
+        self.agent_goals = {}
+        self.box_goals = {}
+
+        for goal in all_goals:
+            if 'A' <= goal.id <= 'Z':
+                # Check if it's a box goal position with the matching ID
+                self.box_goals.append(goal)
+            elif '0' <= goal.id <= '9':
+                # Check if it's an agent goal position with the matching ID
+                self.agent_goals.append(goal)
 
     def h(self, state: 'State') -> 'int':
         #return self.goal_count_heuristic(state)
-        return self.calculate_distance(state)
-        #return self.heuristic_prime_boxes(state)
+        #return self.calculate_distance(state)
+        return self.heuristic_prime_boxes(state)
 
     def calculate_distance(self, state) -> 'int':
         """
@@ -114,16 +122,67 @@ class Heuristic(metaclass=ABCMeta):
             lower the number that this returned, closer are the agents from their destination
         """
         
-        #where is the agent
-        agent = state.agents[0].pos
+        distances = []
+        for agent in state.agents:
                    
-        #Get the goal position of the agent
-        goal = self.agent_goal_position
+            #Get the goal position of the agent
+            for i, goal in enumerate(self.agent_goal):
+                if goal.id == agent.id:
+                    agent_index = i
+                    break
+            goal = self.agent_goals[agent_index]
             
-        # Calculate Manhattan distance if goal position is found
-        distance = abs(agent.pos.x - goal.pos.x) + abs(agent.pos.y - goal.pos.y)
+            # Calculate Manhattan distance if goal position is found
+            distance = abs(agent.pos.x - goal.pos.x) + abs(agent.pos.y - goal.pos.y)
+            distances.append(distance)
+        return distances
+
+    def heuristic_prime_boxes(self, state: "State") -> int:
+
+        total_distance = 0
+        box_indexes = {}
+        current_positions_of_box = []
+        goal_positions_of_box = []
+        box_ids = {}
+        for box in state.boxes:
+            box_ids.append(box.id)
         
-        return distance
+        # Iterate over the keys of self.boxes_goal_position that would be uppercase letters 'A', 'B' etc etc
+        for id in box_ids:    
+            # Get all the goal postions of the same ID
+            for box in self.box_goals:
+                if box.id == id:
+                    goal_positions_of_box.append(box.pos)
+            
+            # Get the current positions for the boxes with the current letter
+            for i, current_box in enumerate(state.boxes):
+                if box.id == current_box.id:
+                    box_indexes.append(i)
+                    current_positions_of_box.append(state.boxes[i].pos)    
+
+            closest_mapping_for_key_boxes = self.find_closest_tuples_manhattan(box.pos,current_positions_of_box)
+
+            for point1, point2 in closest_mapping_for_key_boxes.items():
+                distance = abs(point2[0] - point1[0]) + abs(point2[1] - point1[1])
+                total_distance += distance
+
+        return total_distance
+    
+    def find_closest_tuples_manhattan(self, curr_positions_boxes, desired_positions_boxes):
+        closest_mapping = {}
+        array2_copy = desired_positions_boxes.copy()
+        for point1 in curr_positions_boxes:
+            closest_point = None
+            min_distance = float('inf')
+            for point2 in array2_copy:
+                distance = abs(point2[0] - point1[0]) + abs(point2[1] - point1[1])
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_point = point2
+            closest_mapping[point1] = closest_point
+            curr_positions_boxes.remove(point1)
+            array2_copy.remove(closest_point)
+        return closest_mapping
 
     @abstractmethod
     def f(self, state: 'State') -> 'int': pass
